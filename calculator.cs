6f676bb3e4be0144d38ef2c6a0c99d8a61d9b3c8
allanprakoso <allanprakoso7@gmail.com>
2020-03-10 10:09:30 +0700

    MELENGKAPI FUNGSI MAIN

--- calculator.cs
+++ calculator.cs
@@ -3,6 +3,12 @@ class calculator
        static void Main(string[] args)
 	   {
 		   
+            Console.Title = "Aplikasi Calculator";
+			
+            int a = 10;
+            int b = 6;
+            Console.WriteLine("Hasil Penambahan" + a + " + " + b + " = " + penambahan(a, b));
+            Console.WriteLine("Hasil Pengurangan {0} - {1} = {2}", a, b, pengurangan(a, b));
 	   }
 	   
 	   static int penambahan(int a, int b)
