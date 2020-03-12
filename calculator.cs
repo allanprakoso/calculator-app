38c07ee678961be27f5053e4ed7faf1d0ecd5c10
allanprakoso <allanprakoso7@gmail.com>
2020-03-12 20:03:13 +0700

    menambahkan percabangan switch case

--- calculator.cs
+++ calculator.cs
@@ -2,7 +2,9 @@ using System;
 
 class calculator
 {
-       static void Main(string[] args)
+    private const int V = 1;
+
+    static void Main(string[] args)
 	   {
 		   
         Console.Title = "Aplikasi Calculator";
@@ -16,6 +18,28 @@ class calculator
         int a = int.Parse(Console.ReadLine());
         Console.Write("Nilai b: ");
         int b = int.Parse(Console.ReadLine());
+
+        switch(pilihan)
+        {
+            case 1 :
+            Console.WriteLine("Hasil Penambahan {0} + {1} = {2}", a, b, penambahan(a,b));
+            break;
+            case 2 :
+            Console.WriteLine("Hasil Pengurangan {0} - {1} = {2}", a, b, pengurangan(a,b));
+            break;
+            case 3 :
+            Console.WriteLine("Hasil Pengurangan {0} x {1} = {2}", a, b, perkalian(a,b));
+            break;
+            case 4 :
+            Console.WriteLine("Hasil Pengurangan {0} : {1} = {2}", a, b, pembagian(a,b));
+            break;
+            default:
+            Console.WriteLine("Maaf, menu yang anda pilih tidak tersedia");
+            break;
+        }
+
+        Console.WriteLine("Tekan Sembarang Tombol Untuk Keluar");
+        Console.ReadKey();
 	   }
 	   
 	   static int penambahan(int a, int b)
