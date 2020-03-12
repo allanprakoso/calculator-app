42806f937aba1d517d6e0040b40831c2cc3b73b4
allanprakoso <57885835+allanprakoso@users.noreply.github.com>
2020-03-12 21:06:49 +0700

    Mengatasi Logic Error
    - Membuat Global Variable
    - Membuat Function Input Angka

--- calculator.cs
+++ calculator.cs
@@ -1,44 +1,42 @@
 using System;
-
+namespace Calculator
+{
 class calculator
 {
-    private const int V = 1;
-
+public static int a=0,b=0;
     static void Main(string[] args)
 	   {
 		   
         Console.Title = "Aplikasi Calculator";
-			
         Console.WriteLine("Menu Aplikasi\n");
         Console.WriteLine("1. Penjumlahan \n2. Pengurangan \n3. Perkalian \n4. Pembagian");
-        Console.Write("Pilihan Menu: ");
+        Console.Write("\nPilihan Menu: ");
         int pilihan = int.Parse(Console.ReadLine());
-
-        Console.Write("Nilai a: ");
-        int a = int.Parse(Console.ReadLine());
-        Console.Write("Nilai b: ");
-        int b = int.Parse(Console.ReadLine());
 
         switch(pilihan)
         {
             case 1 :
-            Console.WriteLine("Hasil Penambahan {0} + {1} = {2}", a, b, penambahan(a,b));
+            input();
+            Console.WriteLine("\nHasil Penambahan {0} + {1} = {2}", a, b, penambahan(a,b));
             break;
             case 2 :
-            Console.WriteLine("Hasil Pengurangan {0} - {1} = {2}", a, b, pengurangan(a,b));
+            input();
+            Console.WriteLine("\nHasil Pengurangan {0} - {1} = {2}", a, b, pengurangan(a,b));
             break;
             case 3 :
-            Console.WriteLine("Hasil Pengurangan {0} x {1} = {2}", a, b, perkalian(a,b));
+            input();
+            Console.WriteLine("\nHasil Pengurangan {0} x {1} = {2}", a, b, perkalian(a,b));
             break;
             case 4 :
-            Console.WriteLine("Hasil Pengurangan {0} : {1} = {2}", a, b, pembagian(a,b));
+            input();
+            Console.WriteLine("\nHasil Pengurangan {0} : {1} = {2}", a, b, pembagian(a,b));
             break;
             default:
-            Console.WriteLine("Maaf, menu yang anda pilih tidak tersedia");
+            Console.WriteLine("\nMaaf, menu yang anda pilih tidak tersedia");
             break;
         }
 
-        Console.WriteLine("Tekan Sembarang Tombol Untuk Keluar");
+        Console.WriteLine("\nTekan Sembarang Tombol Untuk Keluar");
         Console.ReadKey();
 	   }
 	   
@@ -58,4 +56,12 @@ class calculator
         {
             return a * b;
         }
+
+        static void input(){
+        Console.Write("\nNilai a: ");
+        a = int.Parse(Console.ReadLine());
+        Console.Write("Nilai b: ");
+        b = int.Parse(Console.ReadLine());
+}
+}
 }
